e95bfae07f1f966222d541ad90fc92fca5c40067
Oscdivin <divineuwandu84@Gmail.com>
2024-08-16 13:22:10 +0100

    CustomiZePage

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 Create your own style with our new 3D Swag Customization App. Pick colors, add logos, and try AI designs to make your virtual swag unique. Built using React.js, Three.js, and OpenAI to show the usage of a 3D world with AI
 
 
-⚙️ Tech Stack
+⚙️ Tech Stack  <>
 React.js
 Three.js
 React Three Fiber
--- src/Pages/CustomiZePage.jsx
+++ src/Pages/CustomiZePage.jsx
@@ -1,3 +1,13 @@
+import  { useState, useEffect} from "react"
+import { AnimatePresence, motion} from "framer-motion"
+import { useSnapshot } from "valtio"
+
+import config from "../config/config"
+import state from "../store"
+import { download} from "../assets"
+import {downloadCanvasToImage, reader} from "../config/helpers"
+import { EditorTabs, FilterTabs, DecalTypes} from "../config/constants"
+import { fadeAnimation, slideAnimation} from "../config/motion"
 
 
 const CustomiZePage = () => {
