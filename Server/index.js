e45518f1b5d1de5b81fb094af61a53a38b193440
Oscdivin <divineuwandu84@Gmail.com>
2024-08-28 00:39:14 +0100

    url index file have something

--- Server/index.js
+++ Server/index.js
@@ -1,1 +1,63 @@
-sddd
+// import express from "express"
+// import * as dotenv from "dotenv"
+// import cors from "cors"
+
+
+// dotenv.config()
+
+// const app = express();
+
+// app.use(cors());
+// app.use(express.json({ limit: "50mb" }))
+
+
+// app.get().then(() => {
+//     console.log('Connected to mongoosedb🚀🚀🚀🚀!');
+
+// }).catch((error) => {
+//     console.log(error);
+// });
+
+// app.get('/', (req, res) => {
+//     res.status(200).json({ message: "Hello  Server 8000 is on" })
+// })
+
+// app.listen(8000, () => {
+//     console.log('Sever is on  port 8000 baby 👋👋👋👋👋👋👋👋👋👋👋👋👋👋👋🏀🏀🏀🏀🏀🏀 ');
+
+// })
+
+
+import express from "express";
+import * as dotenv from "dotenv";
+import cors from "cors";
+import mongoose from "mongoose";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json({ limit: "50mb" }));
+
+// MongoDB connection
+mongoose
+    .connect(process.env.mongoose_URI)
+    .then(() => {
+        console.log("Connected to Sever on port 8000 🏀🏀🏀🏀🏀🏀 🚀🚀🚀🚀!");
+    })
+    .catch((error) => {
+        console.log("Failed to connect to MongoDB", error);
+    });
+
+// Route to test the server
+app.get("/", (req, res) => {
+    console.log(req);
+
+    res.status(200).json({ message: "Hello, Server 8000 is on!" });
+});
+
+// Start the server
+app.listen(8000, () => {
+    console.log('Sever is on  port 8000 baby 👋👋👋👋👋👋👋👋👋👋👋👋👋👋👋🏀🏀🏀🏀🏀🏀 ');
+});
