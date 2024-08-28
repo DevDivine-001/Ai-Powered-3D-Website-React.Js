fbfd07fae73bb0e65c8a67ef3e4879d330743271
Oscdivin <divineuwandu84@Gmail.com>
2024-08-28 23:49:00 +0100

    Dalleroutesis ready added config openapi try and catch

--- Server/routes/dalle.routes.js
+++ Server/routes/dalle.routes.js
@@ -16,22 +16,22 @@ router.route('/').get((req, res) => {
     res.status(200).json({ message: "Hello from DALL.E ROUTES" })
 })
 
-// router.route('/').post(async (req, res) => {
-//     try {
-//         const response = await OpenAI.createImage({
-//             prompt,
-//             n: 1,
-//             size: '1024x1024',
-//             response_format: "b64_json"
-//         })
-//         const image = response.data.data[0].b64_json;
-//         res.status(200).json({ photo: image })
+router.route('/').post(async (req, res) => {
+    try {
+        const response = await OpenAI.createImage({
+            prompt,
+            n: 1,
+            size: '1024x1024',
+            response_format: "b64_json"
+        })
+        const image = response.data.data[0].b64_json;
+        res.status(200).json({ photo: image })
 
-//     } catch (error) {
-//         console.error(error)
-//         res.status(404).json({ message: "Something went wrong" })
+    } catch (error) {
+        console.error(error)
+        res.status(404).json({ message: "Something went wrong" })
 
-//     }
-// })
+    }
+})
 
 export default router
