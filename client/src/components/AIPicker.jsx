12e7a4a766151de9b2ba53db08950cd964c8b12c
Oscdivin <divineuwandu84@Gmail.com>
2024-08-28 23:44:47 +0100

    apipicker file is ready with button and textarea

--- client/src/components/AIPicker.jsx
+++ client/src/components/AIPicker.jsx
@@ -4,13 +4,13 @@ import CustomButton from "./CustomButton"
 const AIPicker = ({ prompt, setPrompt, generatingImg, handleSubmit }) => {
   return (
     <div className="aipicker-container">
-      {/* <textarea name="" id=""
+      <textarea name="" id=""
         placeholder="Ask AI..."
         rows={5}
         value={prompt}
         onChange={(e) => setPrompt(e.target.value)}
         className="aipicker-textarea"
-      /> */}
+      />
       <div className="flex flex-wrap gap-3">
         {
           generatingImg ? (
